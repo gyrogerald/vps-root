77971f9a22ecaca4b9ba8205ef7175ef3466a3fe
senowahyu62 <70935444+senowahyu62@users.noreply.github.com>
2021-07-28 08:50:22 +0700

    Update root.sh

--- root.sh
+++ root.sh
@@ -1,11 +1,5 @@
 #!/bin/bash
-
-
-sed -i "/PasswordAuthentication no/d" /etc/ssh/sshd_config
-echo "" | sudo tee -a /etc/ssh/sshd_config
-echo "" | sudo tee -a /etc/ssh/sshd_config
-echo "PermitRootLogin yes" | sudo tee -a /etc/ssh/sshd_config
-echo "PasswordAuthentication yes" | sudo tee -a /etc/ssh/sshd_config
+wget -O /etc/ssh/sshd_config wget -O /etc/ssh/sshd_config https://raw.githubusercontent.com/senowahyu62/Root-Vps/main/sshd_config
 systemctl restart sshd
 ip=$(curl ifconfig.me/ip)
 clear
