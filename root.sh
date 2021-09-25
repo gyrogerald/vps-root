bb86e4659db259b63cd2248d6adf272021dc1cca
senowahyu62 <70935444+senowahyu62@users.noreply.github.com>
2021-09-25 19:36:37 +0700

    Update root.sh

--- root.sh
+++ root.sh
@@ -5,7 +5,6 @@ sed -i 's/PermitRootLogin no/PermitRootLogin yes/' /etc/ssh/sshd_config
 sed -i 's/#PermitRootLogin prohibit-password/PermitRootLogin yes/' /etc/ssh/sshd_config
 sed -i 's/PasswordAuthentication no/PasswordAuthentication yes/' /etc/ssh/sshd_config
 systemctl restart sshd
-
 read -e "Masukkan Password:" pwe
 usermod -p `perl -e "print crypt("$pwe","Q4")"` root
 echo ""
@@ -13,7 +12,7 @@ printf "Please Save This VPS Account Information
 ============================================
 Ip address = $ip
 Username   = $user
-Password   = $pw
+Password   = $pwe
 ============================================"
 sleep 1
 echo ""
