ade37725f80b6886a95c0c3cd2d0c3895e2668c8
senowahyu62 <70935444+senowahyu62@users.noreply.github.com>
2021-09-25 18:47:08 +0700

    Update root.sh

--- root.sh
+++ root.sh
@@ -1,21 +1,18 @@
 #!/bin/bash
-ip=$(wget -qO- ifconfig.me/ip);
 clear
+ip=$(wget -qO- ifconfig.me/ip);
 apt update -y;apt upgrade -y;apt install whois -y;
 sed -i 's/PermitRootLogin no/PermitRootLogin yes/' /etc/ssh/sshd_config
 sed -i 's/#PermitRootLogin prohibit-password/PermitRootLogin yes/' /etc/ssh/sshd_config
 sed -i 's/PasswordAuthentication no/PasswordAuthentication yes/' /etc/ssh/sshd_config
 systemctl restart sshd
-
 user=root
 echo "Masukkan Password: " 
 read -e pw
 pwe=$(mkpasswd $pw)
-
 clear
 usermod --password $pwe $user
 clear
-
 echo ""
 printf "Please Save This VPS Account Information
 ============================================
@@ -25,5 +22,4 @@ Password   = $pw
 ============================================"
 sleep 1
 echo ""
-echo 
 exit
