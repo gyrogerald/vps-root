f3fae07b0e174135a2e910998ae530ba6386c82b
senowahyu62 <70935444+senowahyu62@users.noreply.github.com>
2021-09-25 18:50:11 +0700

    Update root.sh

--- root.sh
+++ root.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 clear
 ip=$(wget -qO- ifconfig.me/ip);
-apt update -y;apt upgrade -y;apt install whois -y;
+apt update -y;apt install whois -y;
 sed -i 's/PermitRootLogin no/PermitRootLogin yes/' /etc/ssh/sshd_config
 sed -i 's/#PermitRootLogin prohibit-password/PermitRootLogin yes/' /etc/ssh/sshd_config
 sed -i 's/PasswordAuthentication no/PasswordAuthentication yes/' /etc/ssh/sshd_config
